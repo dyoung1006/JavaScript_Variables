854d76023704419a1cdc1cfd610e7f75ad7d85d4
Damien Young <dyoung1006@gmail.com>
2024-09-15 10:08:37 -0700

    completed exercise

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Damien"; //assigning now since its constant and cant be changed later
+let lastName;
+var age;
 
 // Exercise 2
+lastName = "Young";
+age = 44;
+console.log(`${firstName} ${lastName}, and I am ${age} years old.`);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was first released in ${createdYear}.`);
+
+// Excercise 4
+let product = "T-Shirt"; //added product its not in excersie 4 but in video
+let price = 19.99
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(`${product} in size ${selectedSize} is ${price} with ${stock} in stock`);
+
+//Excercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long.  Your last bookmark was page ${bookmark}.`);
